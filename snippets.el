44aa199c5eb613e357b6db05dfbd6f7aa2347527
podhmo <podhmo@example.ne.jp>
2012-05-19 15:29:58 +0900

    added some python snippets

--- snippets.el
+++ snippets.el
@@ -1,32 +1,62 @@
+(eval-when-compile (require 'cl))
+
 (defun snippets:current-directory ()
   (if load-file-name
       (file-name-directory load-file-name)
     default-directory))
-      
+
+(defun snippets:directory-files (directory &optional full nosort)
+  (directory-files directory full "^[^\\.]\\{1,2\\}" nosort))
+
+;; settinsg
+(defvar snippets:current-major-mode nil)
 (defvar snippets:snippet-directory 
-  (concat (snippets:current-directory) ".emacs.d/snippets"))
+  (concat (snippets:current-directory) "snippets"))
+(defvar snippets:snippet-directory-function nil)
+
+(defvar snippets:major-mode-directory-mapping-alist 
+  '((emacs-lisp-mode . "elisp")
+    (python-mode . "python")))
+
+(defun snippets:snippet-directory ()
+  (if snippets:snippet-directory-function
+      (funcall snippets:snippet-directory-function)
+      snippets:snippet-directory))
+
+(defun snippets:get-snippet-dicrectory-by-mode-name ()
+  (let* ((curdir snippets:snippet-directory)
+         (curdir (if (string-equal "/" (substring curdir -2 -1)) curdir (concat curdir "/"))))
+    (concat curdir
+            (assoc-default snippets:current-major-mode
+                           snippets:major-mode-directory-mapping-alist))))
+
+(setq snippets:snippet-directory-function
+      'snippets:get-snippet-dicrectory-by-mode-name)
 
 (defun snippets:snippets-candidates ()
-  (unless (file-exists-p snipetts:snipetts-directory)
-    (make-directory snipetts:snipetts-directory))
-  (directory-files2 snippets:snippet-directory))
-
-(defun snippets-util:insert-snippet (candidate)
-  (let1 fpath (concat snippets:snippet-directory "/" candidate)
+  (let1 dir (snippets:snippet-directory)
+    (unless (file-exists-p dir)
+      (make-directory dir t))
+    (snippets:directory-files dir)))
+;; 
+(defun snippets:insert-snippet (candidate)
+  (let1 fpath (concat (snippets:snippet-directory) "/" candidate)
     (save-restriction
       (narrow-to-region (point) (point))
       (insert-file-contents fpath)
       (goto-char (point-max)))))
 
 (defvar snippets:anything-c-source
-  '((name . "python snippets")
+  '((name . "snippets")
     (candidates . snippets:snippets-candidates)
     (action
-     ("insert" . snippets-util:insert-snippet))
-    (persistent-action . snippets-util:insert-snippet)))
+     ("insert" . snippets:insert-snippet))
+    (persistent-action . snippets:insert-snippet)))
 
 (defun anything-snippets () (interactive)
+  (setq snippets:current-major-mode major-mode)
   (anything-other-buffer 'snippets:anything-c-source
                          "*python snippets*"))
+
 
 (provide 'snippets)
